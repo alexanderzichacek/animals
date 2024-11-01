79a0a238e3332ddc2b3c21c97fe133aa9bc6c5cc
alexanderzichacek <zichacekjunior@gmail.com>
2024-11-01 21:06:52 +0100

    create routes

--- app/Core/RouterFactory.php
+++ app/Core/RouterFactory.php
@@ -6,7 +6,7 @@ namespace App\Core;
 
 use Nette;
 use Nette\Application\Routers\RouteList;
-
+use Nette\Application\Routers\Route;
 
 final class RouterFactory
 {
@@ -15,6 +15,12 @@ final class RouterFactory
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
+
+		$router->addRoute('pet', 'Api:ProcessPetRequest');
+		$router->addRoute('pet/<id>', 'Api:GetById');
+		$router->addRoute('pet/findByTags/<tag>', 'Api:GetByTag');
+		$router->addRoute('pet/findByStatus/<status>', 'Api:GetByStatus');
+
 		$router->addRoute('<presenter>/<action>[/<id>]', 'Home:default');
 		return $router;
 	}
---
+++ app/UI/Api/ApiPresenter.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\UI\Api;
+
+use Nette\Application\Responses\JsonResponse;
+use Nette\Application\UI\Presenter;
+use App\Model\AnimalRepository;
+
+class ApiPresenter extends Presenter
+{
+    private $animalRepository;
+
+    public function __construct()
+    {
+        $this->animalRepository = new AnimalRepository();
+    }
+
+    public function actionGetById(int $id): void
+    {
+        if ($this->getHttpRequest()->getMethod() !== 'GET') {
+            $this->error('Method Not Allowed', 405);
+        }
+        $animalById = $this->animalRepository->findById($id);
+
+        $this->sendResponse(new JsonResponse($animalById, 200));
+    }
+
+    public function actionGetByTag(string $tag): void
+    {
+        if ($this->getHttpRequest()->getMethod() !== 'GET') {
+            $this->error('Method Not Allowed', 405);
+        }
+        $animalsByTag = $this->animalRepository->findByTag($tag);
+
+        $this->sendResponse(new JsonResponse($animalsByTag));
+    }
+
+    public function actionGetByStatus(string $status): void
+    {
+        if ($this->getHttpRequest()->getMethod() !== 'GET') {
+            $this->error('Method Not Allowed', 405);
+        }
+        $animalsByStatus = $this->animalRepository->findByStatus($status);
+
+        $this->sendResponse(new JsonResponse($animalsByStatus));
+    }
+
+    public function actionProcessPetRequest(): void
+    {
+        if ($this->getHttpRequest()->getMethod() === 'GET') {
+            $animals = $this->animalRepository->findAll();
+
+            $this->sendResponse(new JsonResponse($animals));
+        } elseif ($this->getHttpRequest()->getMethod() === 'POST') {
+            $data = json_decode(file_get_contents('php://input'), true);
+
+            $result = $this->animalRepository->createAnimal($data);
+
+            $this->sendResponse(new JsonResponse($result['message'], $result['status']));
+        }
+    }
+
+}
